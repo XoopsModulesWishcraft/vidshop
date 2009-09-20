0c45f9b46d99d9c37a5d27f069769821101ab665
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-20 08:43:28 +0000

    VidShop Alpha (Pre Release - other resources required to finalise)

---
+++ trunk/language/english/main.php
@@ -0,0 +1,6 @@
+<?php
+	
+	define('_VSP_SCOOP_ITEMSADDED', '%s Item(s) Added to Cart.<br/>Cart now has %s items in it!');
+	define('_VSP_FRM_CART_LIST', 'Items in Cart at the moment');
+	define('_VSP_ELE_VIDSHOP_LIST', 'Cart Item %s:')
+?>
